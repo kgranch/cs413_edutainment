d5a3f19efd73452425cfc9c71db45faa4c7dfcb0
Harrison Lambeth <harrison@nau.edu>
2015-02-20 19:50:05 -0700

    Added basic rendering capability for text

--- src/Game.hx
+++ src/Game.hx
@@ -5,11 +5,21 @@ import haxe.io.Bytes;
 import haxe.io.BytesInput;
 import haxe.io.Eof;
 
+import haxe.Timer;
+
 class Game extends Sprite
 {
 	
 	var rootSprite:Sprite;
 	var levelFile:String;
+	
+	var fieldProgress = 0; // Determines what textobject should be popped next
+	var fields:Array<TextObject>; // All of the textobject fields
+	
+	var currentField:TextObject;
+	var renderProgress = 0; // How far in rendering the text animation we are (in characters)
+	
+	var animTimer:Timer;
 	
 	public function new(root:Sprite) {
 		super();
@@ -22,6 +32,19 @@ class Game extends Sprite
 		
 		rootSprite.addChild(this);
 		
+		currentField = popTextObject();
+		
+		animTimer = new Timer(10);
+		animTimer.run = animationTick;
+		
+	}
+	
+	function animationTick() {
+		
+		trace("\n" + currentField.text.substr(0, renderProgress));
+		
+		if (renderProgress < currentField.text.length)
+			renderProgress++;
 	}
 	
 	function load() {
@@ -49,6 +72,16 @@ class Game extends Sprite
 		trace("\n" + fields[0].text);
 		
 		bi.close();
+		
+		this.fields = fields;
+		
+	}
+	
+	function popTextObject():TextObject {
+		renderProgress = 0;
+		if(fieldProgress < this.fields.length)
+			return this.fields[fieldProgress++];
+		return null;
 		
 	}
 }
